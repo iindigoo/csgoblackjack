59561dd11bb9b3b5660fe2297e0aa003407d85a5
Dino Bijdevier <99033225@mydavinci.nl>
2016-07-08 12:50:44 +0200

    Extras
    Design van alle extras is af!

--- application/view/index/index.php
+++ application/view/index/index.php
@@ -79,16 +79,9 @@
 			</div>
 		</nav>
 		<div class="content">
-		<div class="box">
-		<div class="centerImage">
-		<img src="img/test.jpg" alt=""></div>
-		<h1> free coins</h1>
-			<input name="freecoins" placeholder="Enter your code here" type="text" class="form-control ember-view ember-text-field">
-
-			<button name="coinButton" class="btn btn-blackened ember-view">  Redeem Code
-</button>
-		</div>
+		lol
 		<!-- Ajax loaded content here -->
+
 
 		</div>
 	<!-- /view -->
